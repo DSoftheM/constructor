1e24bc6b8ae89d07fbde8f91f38937beade57fc2
DSoftheM <braveartur@mail.ru>
2022-11-22 12:38:43 +0500

    show

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,27 @@ import Main from './components/Main/Main';
 function App() {
 	return (
 		<div className="wrapper">
-			<Header />
-			<Main />
+			{/* <Header />
+			<Main /> */}
+			<F />
 		</div>
 	);
 }
 
 export default App;
+
+const F = () => {
+	return (
+		<ul>
+			<li>1 Пункт</li>
+			<li>2 Пункт</li>
+			<li>3 Пункт</li>
+			<li>
+				<ul>1 подпункт</ul>
+				<ul>2 подпункт</ul>
+				<ul>3 подпункт</ul>
+			</li>
+			<li>4 Пункт</li>
+		</ul>
+	);
+}
